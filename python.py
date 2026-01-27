2f3eb6e82867766212e58b8c106f9e83fd8566f0
git4k <kishoretogo@gmail.com>
2026-01-27 08:11:39 +0530

    Update python.py logic

--- python.py
+++ python.py
@@ -1,5 +1,25 @@
-print('hello world')
-x=2
-y=4
-z=x+y
-print(z)
+def order():
+    food = "rice"
+    print(food)
+
+#order()
+
+def card():
+    discount = 10
+    def checkout():
+        print(discount)
+    checkout()
+#card()
+
+partner = "zomato"
+
+def hotel():
+    item = 'ramen'
+    def order1():
+        food = '12'
+        print(f"{food} {item} {partner}")
+    order1()
+hotel()
+
+
+
